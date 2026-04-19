78a11bd1b474c76d4d047c7b728507d5d32ba196
goddarkmarketing <goddarkmarketing@gmail.com>
2026-04-19 19:17:30 +0700

    Add GitHub Pages deployment support

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
 import { createApp } from './app/App.js';
 
+const GITHUB_PAGES_PATH_KEY = 'gh-pages-spa-path';
+
+function restoreGithubPagesPath() {
+  const pendingPath = window.sessionStorage.getItem(GITHUB_PAGES_PATH_KEY);
+
+  if (!pendingPath) {
+    return;
+  }
+
+  window.sessionStorage.removeItem(GITHUB_PAGES_PATH_KEY);
+  window.history.replaceState({}, '', pendingPath);
+}
+
+restoreGithubPagesPath();
+
 createApp(document.querySelector('#app'));
